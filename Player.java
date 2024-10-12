cca065206ee8b031d08018cce9c701f04dc4af41
Hüma <huma.karapinar@gmail.com>
2024-10-12 15:36:03 +0300

    fixed displayTile error

--- Player.java
+++ Player.java
@@ -118,11 +118,14 @@ public class Player {
     public void displayTiles() {
         System.out.println(playerName + "'s Tiles:");
         for (int i = 0; i < numberOfTiles; i++) {
-            System.out.print(playerTiles[i].toString() + " ");
+            if(playerTiles[i] != null){
+                System.out.print(playerTiles[i].toString() + " ");
+            }
+            
         }
         System.out.println();
     }
-
+ 
     public Tile[] getTiles() {
         return playerTiles;
     }
