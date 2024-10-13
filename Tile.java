efd2a5299085135237698054452a4794163c6a2e
Hüma <huma.karapinar@gmail.com>
2024-10-13 13:36:22 +0300

    fixed null problem

--- Tile.java
+++ Tile.java
@@ -54,9 +54,11 @@ public class Tile {
 
     // determines if this tile can make a chain with the given tile
     public boolean canFormChainWith(Tile t) {
-
+        if(t == null){
+            return false;
+        }
         // can make chain if same number but different color
-        if(t.getColor() != color && t.getValue() == value) {
+        else if(t.getColor() != color && t.getValue() == value) {
             return true;
         } else {
             return false;
