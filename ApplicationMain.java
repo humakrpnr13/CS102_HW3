7abd2128f1fe4b3acb4388a50fc1746613af89e2
Damla Başak Uçar <damlabasakucar14@gmail.com>
2024-10-13 11:19:05 +0300

    a little change in while loop

--- ApplicationMain.java
+++ ApplicationMain.java
@@ -79,7 +79,7 @@ public class ApplicationMain {
                     playerChoice = sc.nextInt();
 
                     // TODO: make sure the given index is correct, should be 0 <= index <= 14
-                    while(!(playerChoice<=14&&playerChoice>=0)){
+                    while((playerChoice>14||playerChoice<0)){
                         System.out.println("Please give an index between 0 and 14: ");
                         playerChoice = sc.nextInt();
                     }
